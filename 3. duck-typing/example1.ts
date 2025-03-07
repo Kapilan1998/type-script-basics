ca00c311abf2e3c204bca79202e1c71366e606b4
Kapilan <sriranjankapilan@gmail.com>
2025-03-07 07:38:49 +0530

    file name modifed

--- 3. duck-typing/example1.ts
+++ 3. duck-typing/example1.ts
@@ -7,4 +7,3 @@ myObj = {name:"hey", id:48, address :"kandy"};
 // duck typing -  same properties must be used in the object when initilization and during reassign
 // if at the initialize stage 4 key value pairs were there. When i reassign or update their value same 4 key value pair must be declared. their value may change
 
-// continue from 29.39  duck type finished
--- 6. enums/enum.ts
+++ 6. enums/enum.ts
@@ -10,3 +10,4 @@ function move(direction) {
 }
 
 move(Direction.East);
+
